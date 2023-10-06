662a59b7b8b67969277d10a4be191ae980203af3
sseladiya <sagar.s@yalabs.in>
2023-10-06 14:51:01 +0530

    [ADD]: Mongo DB connected successfully

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI) 
+    .then(() => {
+        console.log('Hello Sagar Seladiya');
+      })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+      });
+    
+}
+
+module.exports = connectToMongo
