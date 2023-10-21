a9dc019a80378d12c299cb8c4feeff9cdb31d16c
sseladiya <sagar.s@yalabs.in>
2023-10-22 01:59:59 +0530

    Delete note by click on delete icon

--- src/components/Notesitem.js
+++ src/components/Notesitem.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import NoteContext from "../context/notes/NoteContext";
+
 
 const Notesitem = (props) => {
+  const context = useContext(NoteContext)
+  const { deleteNote } = context
   const { note } = props;
   return (
     <>
@@ -8,7 +12,7 @@ const Notesitem = (props) => {
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">
-              {note.title} <i className="fa-solid fa-trash mx-2"></i>
+              {note.title} <i className="fa-solid fa-trash mx-2" onClick={() =>{deleteNote(note._id)}}></i>
               <i className="mx-2 fa-solid fa-pen-to-square"></i>
             </h5>
             <p className="card-text">{note.description}</p>
