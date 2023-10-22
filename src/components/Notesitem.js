c4de5e429a0ff3cb4df467fc0c861a167934e0b4
sseladiya <sagar.s@yalabs.in>
2023-10-23 01:08:30 +0530

    Add note fucntionality, Fetch Note functionality and Edit Note dunctionality DONE

--- src/components/Notesitem.js
+++ src/components/Notesitem.js
@@ -5,7 +5,7 @@ import NoteContext from "../context/notes/NoteContext";
 const Notesitem = (props) => {
   const context = useContext(NoteContext)
   const { deleteNote } = context
-  const { note } = props;
+  const { note, updateNote } = props;
   return (
     <>
       <div className="col-md-4 my-3">
@@ -13,7 +13,7 @@ const Notesitem = (props) => {
           <div className="card-body">
             <h5 className="card-title">
               {note.title} <i className="fa-solid fa-trash mx-2" onClick={() =>{deleteNote(note._id)}}></i>
-              <i className="mx-2 fa-solid fa-pen-to-square"></i>
+              <i className="mx-2 fa-solid fa-pen-to-square" onClick={()=>{updateNote(note)}}></i>
             </h5>
             <p className="card-text">{note.description}</p>
           </div>
